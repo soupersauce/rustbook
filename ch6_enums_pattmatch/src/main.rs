9e79fcf94d0219a0e9807215676e24bf68c63696
soupersauce <soupersauce@gmail.com>
2019-05-21 17:13:20 -0700

    Finished first section of ch6

--- ch6_enums_pattmatch/src/main.rs
+++ ch6_enums_pattmatch/src/main.rs
@@ -3,13 +3,29 @@ enum IpAddr {
     V6(String),
 }
 
+enum Message {
+    Quit,
+    Move { x:i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        // method body definition
+    }
+}
+
 fn main() {
    let home = IpAddr::V4(127, 0, 0, 1);
 
-   let loopback = IpAddr::V6(String::from("::1"))
+   let loopback = IpAddr::V6(String::from("::1"));
+
+   let m = Message::Write(String::from("hello"));
+   m.call();
 }
 
 
-fn route(ip_type: IpAddrKind) {
+fn route(ip_type: IpAddr) {
 //TODO
 }
