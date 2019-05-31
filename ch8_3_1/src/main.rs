c376e65bfb1b40503049b257697d0fd60dd9c2e9
soupersauce <soupersauce@gmail.com>
2019-05-30 19:37:01 -0700

    continued 8_3

--- ch8_3_1/src/main.rs
+++ ch8_3_1/src/main.rs
@@ -5,5 +5,12 @@ fn main() {
     let mut scores = HashMap::new();
 
     scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yello"), 50);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
 }
