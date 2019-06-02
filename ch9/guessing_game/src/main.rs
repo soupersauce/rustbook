410e49bb20e4fecee69b5ada422979c1e7909dfe
soupersauce <soupersauce@gmail.com>
2019-06-01 17:57:42 -0700

    Finished ch9

---
+++ ch9/guessing_game/src/main.rs
@@ -0,0 +1,57 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("Please input your guess:");
+
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input)
+            .expect("Failed to read line");
+
+        let input: i32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        let guess = Guess::new(input);
+
+        println!("You guessed: {}", guess.value());
+
+        match guess.value().cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
---
+++ ch9/result-2/src/main.rs
@@ -0,0 +1,9 @@
+use std::fs::File;
+
+// fn main() {
+//     let f = File::open("hello.txt").unwrap();
+// }
+
+fn main() {
+    let f = File::open("hello.txt").expect("Failed to open hello.txt");
+}
