7ec20e4933f5f79150dc5fdfd05eec8287a8f802
soupersauce <soupersauce@gmail.com>
2019-06-10 17:18:24 -0700

    Completed ch10

--- ch10/lifetimes-3/src/main.rs
+++ ch10/lifetimes-3/src/main.rs
@@ -2,6 +2,17 @@ struct ImportantExcerpt<'a> {
     part: &'a str,
 }
 
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}" announcement);
+        self.part
+    }
+}
+
 fn main() {
     let novel = String::from("Call me Ishmael. Some years ago...");
     let first_sentence = novel.split('.')
