03d437c4cff25b7d5047ec4e482e00597ab9a051
soupersauce <soupersauce@gmail.com>
2019-06-18 15:18:43 -0700

    completed ch18

---
+++ ch18/pattsyntax-3/src/main.rs
@@ -0,0 +1,29 @@
+struct Point {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+
+fn main() {
+    let origin = Point { x: 0, y: 0, z:0 };
+    match origin {
+        Point { x, .. } => println!("x is {}", x),
+    }
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {}, {}", first, last);
+        },
+    }
+
+    let num = Some(4);
+
+    match num {
+        Some(x) if x < 5 => println!("less than five: {}", x),
+        Some(x) => println!("{}", x),
+        None => (),
+    }
+}
