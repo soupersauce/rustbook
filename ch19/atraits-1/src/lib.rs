2cf337900ec7998f4fdbad0b6e557219b883eb3a
soupersauce <soupersauce@gmail.com>
2019-06-20 00:02:51 -0700

    completed ch19

--- ch19/atraits-1/src/lib.rs
+++ ch19/atraits-1/src/lib.rs
@@ -2,12 +2,24 @@ use std::ops::Add;
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn add_meters_to_millimeters() {
+        let milli = Millimeters(25);
+        let meter = Meters(1);
+
+        assert_eq!(Millimeters(1025), milli + meter );
+    }
+    #[test]
+    fn fail_add_meters_to_millimeters() {
+        let milli = Millimeters(25);
+        let meter = Meters(1);
+
+        assert_eq!(Millimeters(1026), milli + meter );
     }
 }
 
+#[derive(Debug, PartialEq)]
 struct Millimeters(u32);
 struct Meters(u32);
 
---
+++ ch19/macros-1/src/lib.rs
@@ -0,0 +1,6 @@
+use proc_macro;
+
+#[some_attribute]
+pub fn some_name(input: TokenStream) -> TokenStream {
+
+}
